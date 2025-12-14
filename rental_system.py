4ccff18fcef97084cc2a489771855457b9125565
dilliram-code <chaudharydr23@gmail.com>
2025-12-14 22:17:14 +0545

    feat: add register customer

--- rental_system.py
+++ rental_system.py
@@ -15,4 +15,18 @@ class CustomerNotFoundErro(Exception):
 class RentalNotFoundError(Exception):
     pass
 
-
+class RentalSystem:
+    def __init__(self, pricing_strategy: PricingStrategy = None):
+        self.vehicles = {}
+        self.customers = {}
+        self.rentals = {}
+        self.pricing_strategy = pricing_strategy or FlatRateStrategy()
+        
+    # crud helpers
+    def add_vehicle(self, vehicle: Vehicle):
+        self.vehicles[vehicle.id] = vehicle
+    
+    def register_customer(self, customer: Customer):
+        if not customer.can_rent():
+            raise ValueError("Customer must have a valid license.")
+        self.customers[customer.id] = customer
