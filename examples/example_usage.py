3100c30368c0a0ecdb2c4755880ea4c056300b37
dilliram-code <chaudharydr23@gmail.com>
2025-12-30 22:59:21 +0545

    add register customer functionality

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -10,5 +10,10 @@ system = RentalSystem(pricing_strategy=TieredRateStrategy())
 car = Car(id="", make="Toyota", model="Corolla", year=2020, base_rate_per_day=50.0)
 suv = SUV(id="", make="Hyundai", model="Palisade", year=2021, base_rate_per_day=80.0, four_wheel_drive=True)
 bike = Bike(id="", make="Yamaha", model="R15", year=2019, base_rate_per_day=20.0)
+
 for v in [car, suv, bike]:
     system.add_vehicle(v)
+
+# register customer
+customer = Customer(id="", name="Alisha", license_number="L-12345")
+system.register_customer(customer)
